bd6ce290f520ded7e2637f1be941f1c1f9f9a273
sebastianbakala <sebastianbakala@users.noreply.github.com>
2017-05-29 13:42:47 +0200

    Update ljc.h

--- ljc.h
+++ ljc.h
@@ -6,10 +6,11 @@
       struct ls_t* next;
       int value; //PRZEROBIĆ NA WSKAŹNIKI DO FUNKCJI
     };
-    ls_t* init_list(ls_t*);
+    ls_t* init_list(ls_t*, int);
     ls_t* list_add_begin(ls_t*, int);
     ls_t* list_add_end(ls_t*, int);
     ls_t* list_del_node(ls_t*, int);
+    ls_t* list_clear(ls_t*);
     unsigned int list_size(ls_t*);
     void list_view(ls_t*);
 #endif
