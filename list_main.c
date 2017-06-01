404d8a07dc37fc20331ae399df40f64023642b3d
sebastianbakala <sebastianbakala@users.noreply.github.com>
2017-06-01 20:45:05 +0200

    Update list_main.c

--- list_main.c
+++ list_main.c
@@ -5,7 +5,7 @@ int main(void)
 {
   ls_t* root;
   root = init_list(root, 0);
-  root = list_add_begin(root, 10);
+  root = list_add_begin(list_add_begin(root, 10), 90);
   root = list_add_begin(root, 20);
   root = list_add_begin(root, 30);
   root = list_add_begin(root, 40);
@@ -13,22 +13,13 @@ int main(void)
   root = list_add_end(root, 60);
   root = list_add_end(root, 70);
   root = list_add_end(root, 80);
-  printf("list_size = %d\n", list_size(root));
+  printf("\n\nlist_size = %d\n", list_size(root));
   list_view(root);
-  root = list_del_node(root, 4);
-  list_view(root);
-  root = list_del_node(root, 5);
-  list_view(root);
-  root = list_del_node(root, 5);
-  list_view(root);
-  root = list_del_node(root, 5);
-  list_view(root);
-  root = list_del_node(root, 4);
-  list_view(root);
-  root = list_del_node(root, 1);
-  list_view(root);
-  list_clear(root);
-  list_view(root);
+  //list_size(root);
+
+  // list_search_with_guard(root, 80);
+  // list_search(root, 81);
+  root = insertion_sort(root);
 
   return 0;
 }
