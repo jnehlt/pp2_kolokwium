b7bb83d6fe0020f8bcf455c99607fc3052ccef05
sebastianbakala <sebastianbakala@users.noreply.github.com>
2017-06-01 20:44:29 +0200

    Update ljc.c

--- ljc.c
+++ ljc.c
@@ -12,6 +12,11 @@ ls_t* init_list(ls_t* r, int value)
 
 unsigned int list_size(ls_t* r)
 {
+  if (r == NULL)
+  {
+    printf("\nLIST IS EMPTY\n");
+    return 0;
+  }
   ls_t* loop = r;
   int i = 1;
   while(r->next != loop)
@@ -24,6 +29,10 @@ unsigned int list_size(ls_t* r)
 
 void list_view(ls_t* r)
 {
+  if (r == NULL)
+  {
+    return;
+  }
   ls_t* loop = r;
   printf("\n+=+=+=+=+=+=+=+=+=+=+ LIST VIEW +=+=+=+=+=+=+=+=+=+=+\n");
   while(r->next != loop)
@@ -97,7 +106,7 @@ ls_t* list_del_node(ls_t* r, int which)
         if(r == root) //we've got an loop...
         {
           printf("ERROR_2\nyou expect too much from me\n");
-          return root;
+          goto RETURN;
         }
         ++i;
       }
@@ -105,7 +114,6 @@ ls_t* list_del_node(ls_t* r, int which)
       while(r->next != temp)
       {
         r = r->next;
-        printf("r = %p\n",r);
       }
       r->next = r->next->next;
       if(temp == root)  //if deleting first element
@@ -124,16 +132,109 @@ ls_t* list_del_node(ls_t* r, int which)
   }
 }
 
-ls_t* list_clear(ls_t* r)
+void list_clear(ls_t** r)
 {
-  //DOIT!!!!
-  // ls_t* temp;
-  // while(r->next != NULL)
-  // {
-  //   temp = r;
-  //   r = r->next;
-  //   free(temp);
-  // }
+  int size = list_size(*r);
+  ls_t* temp;
+  while(size--)
+  {
+    temp = *r;
+    *r = (*r)->next;
+    free(temp);
+  }
+  *r = NULL;
+}
 
-  return r;
+void list_search_with_guard(ls_t* r, int value)
+{
+  if(r != NULL)
+  {
+    //add guard at the end
+    ls_t* loop = list_add_end(r, value);
+    while(r->value != value)
+    {
+      r = r->next;
+    }
+    if(r->next == loop) //not found
+    {
+      printf("\nSzukany element [%d] nie zostal odnaleziony.\n", value);
+    }
+    else
+    {
+      printf("\nOznaleziono element [%d] pod adresem %p.\n", value, r);
+    }
+
+    list_del_node(loop, list_size(loop));
+  }
+}
+
+void list_search(ls_t* r, int value)
+{
+  if(r != NULL)
+  {
+    ls_t* loop = list_add_end(r, value);
+    while(r->value != value)
+    {
+      r = r->next;
+    }
+    if(r->next == loop)
+    {
+      printf("\nSzukany element [%d] nie zostal odnaleziony.\n", value);
+    }
+    else
+    {
+      printf("\nOznaleziono element [%d] pod adresem %p.\n", value, r);
+    }
+    list_del_node(loop, list_size(loop));
+  }
+}
+
+ls_t* insertion_sort(ls_t* r)
+{
+  int size = list_size(r), i = 0;
+  ls_t* buffer;
+  ls_t* root = r;
+
+  while(size) //how much iterations
+  {
+    int j = 0;
+    while(j < i)
+    {
+      r = r->next;
+      ++j;
+//      printf("i = %d   j = %d    ", i, j);
+    }
+    ls_t* to_swap = r;
+    //printf("size = %d\n", size);
+    ++i;
+
+    buffer = r;
+    j = 0;
+    while(j < size)  //find max till root
+    {
+      printf("r = %p value = %d    ", r, r->value);
+      if(r->value > buffer->value)  //remeber that adress
+      {
+        buffer = r;
+      }
+      r = r->next;
+      printf("buffer = %p\n", buffer);
+      ++j;
+    }
+    //max found! now swap.
+    j = 0;
+    while(j < i-1)
+    {
+      r = r->next;
+      ++j;
+    }
+    ls_t* buffer2;
+    buffer2 = r;
+    printf("buffer2 = %d    ",buffer2->value);
+    printf("to_swap = %d\n"  , to_swap->value);
+
+
+    --size;
+  }
+  return root;
 }
